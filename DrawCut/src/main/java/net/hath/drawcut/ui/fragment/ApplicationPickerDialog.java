d6440a5af3038fbb630d93bc0683023543f1cbf1
Martin Thoresen <martinhath@gmail.com>
2014-01-07 22:20:07 +0100

    Improved performance of the applicationList.
    Next, fade in animation when the application icons are first loaded.

--- DrawCut/src/main/java/net/hath/drawcut/ui/fragment/ApplicationPickerDialog.java
+++ DrawCut/src/main/java/net/hath/drawcut/ui/fragment/ApplicationPickerDialog.java
@@ -6,10 +6,12 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+
 import net.hath.drawcut.R;
 import net.hath.drawcut.ui.activitiy.NewGestureActivity;
 import net.hath.drawcut.ui.adapter.ApplicationAdapter;
@@ -21,6 +23,8 @@ import java.util.List;
 
 public class ApplicationPickerDialog extends DialogFragment {
 
+    GridView gridView;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -29,8 +33,9 @@ public class ApplicationPickerDialog extends DialogFragment {
 
         View view = getActivity().getLayoutInflater().inflate(R.layout.application_list, null);
         builder.setView(view);
+        gridView = (GridView) view.findViewById(R.id.application_grid);
 
-        ((GridView) view.findViewById(R.id.application_grid)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 NewGestureActivity activity = (NewGestureActivity) getActivity();
@@ -39,24 +44,37 @@ public class ApplicationPickerDialog extends DialogFragment {
             }
         });
 
-        List<ApplicationInfo> items = getActivity().getPackageManager()
-                .getInstalledApplications(PackageManager.GET_META_DATA);
-        if (items == null) {
-            throw new NullPointerException();
-        }
-        List<ApplicationInfo> filteredItems = new ArrayList<ApplicationInfo>();
-        PackageManager pm = getActivity().getPackageManager();
-        for (ApplicationInfo ai : items) {
-            Intent i = pm.getLaunchIntentForPackage(ai.packageName);
-            if (i != null) {
-                filteredItems.add(ai);
-            }
-        }
-        Collections.sort(filteredItems, new ApplicationInfoComparator(pm));
-        GridView gw = (GridView) view.findViewById(R.id.application_grid);
-        gw.setAdapter(new ApplicationAdapter(getActivity(), R.layout.application_item, R.id.app_name, filteredItems));
+        new LoadingTask().execute();
 
         return builder.create();
+    }
+
+    private class LoadingTask extends AsyncTask<Void, Void, List<ApplicationInfo>> {
+
+        @Override
+        protected List<ApplicationInfo> doInBackground(Void... voids) {
+            List<ApplicationInfo> items = getActivity().getPackageManager()
+                    .getInstalledApplications(PackageManager.GET_META_DATA);
+            if (items == null) {
+                throw new NullPointerException();
+            }
+            List<ApplicationInfo> filteredItems = new ArrayList<ApplicationInfo>();
+            PackageManager pm = getActivity().getPackageManager();
+            for (ApplicationInfo ai : items) {
+                Intent i = pm.getLaunchIntentForPackage(ai.packageName);
+                if (i != null) {
+                    filteredItems.add(ai);
+                }
+            }
+            Collections.sort(filteredItems, new ApplicationInfoComparator(pm));
+            return filteredItems;
+        }
+
+        @Override
+        protected void onPostExecute(List<ApplicationInfo> applicationInfos) {
+            super.onPostExecute(applicationInfos);
+            gridView.setAdapter(new ApplicationAdapter(getActivity(), R.layout.application_item, R.id.app_name, applicationInfos));
+        }
     }
 
 }
