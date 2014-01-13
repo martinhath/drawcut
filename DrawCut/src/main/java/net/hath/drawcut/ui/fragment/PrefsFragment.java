2980f547d73037abe5fdfcbcc1f1aab1ece94085
Martin Thoresen <martinhath@gmail.com>
2014-01-13 22:13:49 +0100

    Toggling the floater now works.
    PrefsFragment listens to changes in SharedPreferences.

--- DrawCut/src/main/java/net/hath/drawcut/ui/fragment/PrefsFragment.java
+++ DrawCut/src/main/java/net/hath/drawcut/ui/fragment/PrefsFragment.java
@@ -1,15 +1,19 @@
 package net.hath.drawcut.ui.fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import net.hath.drawcut.R;
+import net.hath.drawcut.service.HUD;
 
 /**
  * Created by Martin on 13.01.14.
  */
-public class PrefsFragment extends PreferenceFragment {
+public class PrefsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     private static final String TAG = "PrefsFragment";
 
     @Override
@@ -19,6 +23,26 @@ public class PrefsFragment extends PreferenceFragment {
         prefm.setSharedPreferencesName("settings");
         prefm.setSharedPreferencesMode(getActivity().MODE_PRIVATE);
 
+        prefm.getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        Log.d(TAG, "Preferences are changed");
+        if (s.equals("service_enabled")) {
+            boolean shouldStart = sharedPreferences.getBoolean(s, false);
+            if (shouldStart) {
+                // Turn on
+                Intent intent = new Intent(getActivity(), HUD.class);
+                getActivity().startService(intent);
+            } else {
+                // Turn off
+                Intent intent = new Intent(getActivity(), HUD.class);
+                getActivity().stopService(intent);
+            }
+
+        }
     }
 }
